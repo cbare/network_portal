7ce40d0db79477cc308e424ebe7574706feba334
kfoley <kfoley73@gmail.com>
2011-12-01 09:27:52 -0800

    Added the displaying/showing of functions for genes to search results page

--- web_app/views.py
+++ web_app/views.py
@@ -7,6 +7,7 @@ from django.template import RequestContext
 from django.contrib.auth import logout
 
 import search as s
+import itertools
 
 def home(request):
     #return render_to_response('home.html', locals())
@@ -26,13 +27,30 @@ def search(request):
         bicluster_ids = []
         biclusters = []
         genes = []
+        gene_functions = {}
+        terms = []
         for result in results:
             if result['doc_type']=='BICLUSTER':
                 biclusters.append(result)
                 if 'bicluster_id' in result and result['bicluster_id'] not in bicluster_ids:
                     bicluster_ids.append(result['bicluster_id'])
             elif result['doc_type']=='GENE':
+                # create sorted list of functions per gene; 
+                # use if user wants to display Gene: Functions
+                functions = zip(result['gene_function_type'], result['gene_function_name'])
+                gene_functions[result['gene_name']] = functions
+                ret = sorted(gene_functions.items())
+                # create a list of unique functions found among the collection
+                # of genes queried by user
+                terms.append(result['gene_function_name'])
+                terms_list = list(itertools.chain.from_iterable(terms))
+                terms_lc = map(lambda x:x.lower(), terms_list)
+                unique = set(terms_lc)
+                unique = list(unique)
+                unique.sort()
+                # all gene results together
                 genes.append(result)
+
     return render_to_response('search.html', locals())
 
 def logout_page(request):
